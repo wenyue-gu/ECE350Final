283c3c9f6a24b834cf4155bb91b1307700c70783
Cady <cadyzhou@outlook.com>
2021-11-12 13:52:25 -0500

    add leds for testing

--- constraints.xdc
+++ constraints.xdc
@@ -13,5 +13,23 @@ set_property PACKAGE_PIN D14 [get_ports in1]
 set_property IOSTANDARD LVCMOS33 [get_ports in1]
 
 # LED
-set_property PACKAGE_PIN V11 [get_ports led1]
+set_property PACKAGE_PIN V11 [get_ports led15]
+set_property IOSTANDARD LVCMOS33 [get_ports led15]
+
+set_property PACKAGE_PIN H17 [get_ports led0]
+set_property IOSTANDARD LVCMOS33 [get_ports led0]
+
+set_property PACKAGE_PIN K15 [get_ports led1]
 set_property IOSTANDARD LVCMOS33 [get_ports led1]
+
+set_property PACKAGE_PIN J13 [get_ports led2]
+set_property IOSTANDARD LVCMOS33 [get_ports led2]
+
+set_property PACKAGE_PIN N14 [get_ports led3]
+set_property IOSTANDARD LVCMOS33 [get_ports led3]
+
+set_property PACKAGE_PIN R18 [get_ports led4]
+set_property IOSTANDARD LVCMOS33 [get_ports led4]
+
+set_property PACKAGE_PIN V17 [get_ports led5]
+set_property IOSTANDARD LVCMOS33 [get_ports led5]
--- test.v
+++ test.v
@@ -1,20 +1,43 @@
+`timescale 1s/1s
 module test (
     output reg o1,
-    output [4:0] score,
-    //output led1,
-    input in1
-    //input clk,
+    output reg[2:0] score,
+    input in1,
+    input clk,
+
+    output reg led0,
+    output reg led1,
+    output reg led2,
+    output reg led3,
+    output reg led4,
+    output reg led5
 );
-    initial 
+    initial begin
         o1 = 1'b1;
-    //assign led1 = in1;
-    reg [4:0] score = 5'd0;
+        score = 3'd0;
+    end 
+
     always @(posedge in1) begin
         if (o1) begin
             o1 = 1'b0;
-            score <= score + 5'd1;
-            #100
+            score <= score + 3'd1;
+            #1
             o1 = 1'b1;
         end
+    end
+
+    always @(posedge clk) begin
+        if (score == 3'd0)
+            led0 = 1'b1;
+        if (score == 3'd1)
+            led1 = 1'b1;
+        if (score == 3'd2)
+            led2 = 1'b1;
+        if (score == 3'd3)
+            led3 = 1'b1;
+        if (score == 3'd4)
+            led4 = 1'b1;
+        if (score == 3'd5)
+            led5 = 1'b1;
     end
 endmodule
