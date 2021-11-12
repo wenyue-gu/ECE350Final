0f6d8ba9879cc6a48b03519fd7008a1e55dffaff
Cady Zhou <zz160@duke.edu>
2021-11-12 14:38:39 -0500

    day 1

--- test.v
+++ test.v
@@ -1,7 +1,7 @@
 `timescale 1s/1s
 module test (
     output reg o1,
-    output reg[2:0] score,
+    
     input in1,
     input clk,
 
@@ -12,9 +12,16 @@ module test (
     output reg led4,
     output reg led5
 );
+    reg[2:0] score;
     initial begin
         o1 = 1'b1;
         score = 3'd0;
+        led0 = 1'b0;
+        led1 = 1'b0;
+        led2 = 1'b0;
+        led3 = 1'b0;
+        led4 = 1'b0;
+        led5 = 1'b0;
     end 
 
     always @(posedge in1) begin
@@ -28,16 +35,16 @@ module test (
 
     always @(posedge clk) begin
         if (score == 3'd0)
-            led0 = 1'b1;
+            led0 <= 1'b1;
         if (score == 3'd1)
-            led1 = 1'b1;
+            led1 <= 1'b1;
         if (score == 3'd2)
-            led2 = 1'b1;
+            led2 <= 1'b1;
         if (score == 3'd3)
-            led3 = 1'b1;
+            led3 <= 1'b1;
         if (score == 3'd4)
-            led4 = 1'b1;
+            led4 <= 1'b1;
         if (score == 3'd5)
-            led5 = 1'b1;
+            led5 <= 1'b1;
     end
 endmodule
