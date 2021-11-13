cd519f9f5805723d57c7572f10a61a9b0ac17757
Cady Zhou <zz160@duke.edu>
2021-11-13 18:04:02 -0500

    blink

--- test.v
+++ test.v
@@ -1,4 +1,3 @@
-`timescale 1s/1s
 module test (
     output reg o1,
     
@@ -10,12 +9,14 @@ module test (
     output reg led2,
     output reg led3,
     output reg led4,
-    output reg led5
+    output reg led5,
+    output reg led15
 );
     reg[2:0] score;
-    reg[3:0] counter;
+    integer counter;
+
     initial begin
-        counter = 4'b0;
+        counter = 0;
         o1 = 1'b1;
         score = 3'd0;
         led0 = 1'b0;
@@ -24,43 +25,52 @@ module test (
         led3 = 1'b0;
         led4 = 1'b0;
         led5 = 1'b0;
+        led15 = 1'b0;
     end 
 
-    always @(posedge in1) begin
-        if (o1) begin
-            o1 = 1'b0;
-            counter<=4'd0;
-            score <= score + 3'd1;
-        end
-    end
+//    always @(posedge in1) begin
+//        if (o1) begin
+//            o1 <= 1'b0;
+//            counter = 0;
+//            score <= score + 3'd1;
+//        end
+//    end
 
     always @(posedge clk) begin
-        counter <= counter+4'd1;
-        if (counter== 4'd10) begin
-            counter<=4'd0;
-            o1=~o1;
+       
+       if (counter >= 50000000) begin
+           counter <= 0;
+           o1 <= !o1;
+           end
+       else
+       counter = counter + 1;
+       if (in1==1'b0 && o1==1'b1) begin
+            o1=1'b0;
+            counter=0;
+            score <= score + 3'd1;
         end
-        if (score%6 == 3'd0)
+        
+        if (score % 6 == 3'd0)
             led0 <= 1'b1;
         else
             led0 <= 1'b0;
-        if (score%6 == 3'd1)
+        if (score % 6 == 3'd1)
             led1 <= 1'b1;
         else
             led1 <= 1'b0;
-        if (score%6 == 3'd2)
+        if (score % 6 == 3'd2)
             led2 <= 1'b1;
         else
             led2 <= 1'b0;
-        if (score%6 == 3'd3)
+        if (score % 6 == 3'd3)
             led3 <= 1'b1;
         else
             led3 <= 1'b0;
-        if (score%6 == 3'd4)
+        if (score % 6 == 3'd4)
             led4 <= 1'b1;
         else
             led4 <= 1'b0;
-        if (score%6 == 3'd5)
+        if (score % 6 == 3'd5)
             led5 <= 1'b1;
         else
             led5 <= 1'b0;
