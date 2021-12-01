bd5acd89f047b1b78f54804f7728fd0784b5de06
Cady Zhou <zz160@duke.edu>
2021-12-01 14:24:45 -0500

    ,

--- vivado_project/vivado_project.runs/impl_1/rundef.js
+++ vivado_project/vivado_project.runs/impl_1/rundef.js
@@ -23,9 +23,9 @@ eval( EAInclude(ISEJScriptLib) );
 
 
 // pre-commands:
-ISETouchFile( "init_design", "begin" );
+ISETouchFile( "write_bitstream", "begin" );
 ISEStep( "vivado",
-         "-log main.vdi -applog -m64 -product Vivado -messageDb vivado.pb -mode batch -source main.tcl -notrace" );
+         "-log test.vdi -applog -m64 -product Vivado -messageDb vivado.pb -mode batch -source test.tcl -notrace" );
 
 
 
--- vivado_project/vivado_project.runs/impl_1/runme.sh
+++ vivado_project/vivado_project.runs/impl_1/runme.sh
@@ -41,7 +41,7 @@ EAStep()
 }
 
 # pre-commands:
-/bin/touch .init_design.begin.rst
-EAStep vivado -log main.vdi -applog -m64 -product Vivado -messageDb vivado.pb -mode batch -source main.tcl -notrace
+/bin/touch .write_bitstream.begin.rst
+EAStep vivado -log test.vdi -applog -m64 -product Vivado -messageDb vivado.pb -mode batch -source test.tcl -notrace
 
 
