13072a892fdf4ed97bd2cfa727914d0507ce2895
Lucy Gu <wg74@duke.edu>
2021-11-30 19:15:32 -0500

    remove comment

--- test.v
+++ test.v
@@ -1,16 +1,16 @@
-// module test (
-//     output reg o1,
-//     input in1,
-//     output reg o2,
-//     input in2,
-//     output reg o3,
-//     input in3,
-//     output reg o4,
-//     input in4,
-//     output reg o5,
-//     input in5,
+module test (
+    output reg o1,
+    input in1,
+    output reg o2,
+    input in2,
+    output reg o3,
+    input in3,
+    output reg o4,
+    input in4,
+    output reg o5,
+    input in5,
 
-//     input clk,
+    input clk,
 
     output reg led0,
     output reg led1,
@@ -68,65 +68,65 @@
         led15 = 1'b0;
     end 
 
-// // 100 000000 - 1s
-//     always @(posedge clk) begin
-//         if (ingame==1'b1) begin
+// 100 000000 - 1s
+    always @(posedge clk) begin
+        if (ingame==1'b1) begin
 
-//             //button 1
-//             if (o1==1'b1 && counter1 >= 50000000) begin
-//                 counter1 <= 0;
-//                 o1 <= 1'b0;
-//             end else if(o1==1'b0 && counter1 >= 250000000) begin
-//                 counter1 <= 0;
-//                 o1 <= 1'b1;
-//             end
-//             if (in1 != in1m && in1==1'b0 && o1==1'b1) begin
-//                 o1=1'b0;
-//                 counter1=0;
-//                 score <= score + 3'd1;
-//             end
+            //button 1
+            if (o1==1'b1 && counter1 >= 50000000) begin
+                counter1 <= 0;
+                o1 <= 1'b0;
+            end else if(o1==1'b0 && counter1 >= 250000000) begin
+                counter1 <= 0;
+                o1 <= 1'b1;
+            end
+            if (in1 != in1m && in1==1'b0 && o1==1'b1) begin
+                o1=1'b0;
+                counter1=0;
+                score <= score + 3'd1;
+            end
             
-//             //button 2
-//             if (o2==1'b1 && counter2 >= 50000000) begin
-//                 counter2 <= 0;
-//                 o2 <= 1'b0;
-//             end else if(o2==1'b0 && counter2 >= 350000000) begin
-//                 counter2 <= 0;
-//                 o2 <= 1'b1;
-//             end
-//             if (in2 != in2m && in2==1'b0 && o2==1'b1) begin
-//                 o2=1'b0;
-//                 counter2=0;
-//                 score <= score + 3'd1;
-//             end
+            //button 2
+            if (o2==1'b1 && counter2 >= 50000000) begin
+                counter2 <= 0;
+                o2 <= 1'b0;
+            end else if(o2==1'b0 && counter2 >= 350000000) begin
+                counter2 <= 0;
+                o2 <= 1'b1;
+            end
+            if (in2 != in2m && in2==1'b0 && o2==1'b1) begin
+                o2=1'b0;
+                counter2=0;
+                score <= score + 3'd1;
+            end
 
-//             //button 3
-//             if (o3==1'b1 && counter3 >= 50000000) begin
-//                 counter3 <= 0;
-//                 o3 <= 1'b0;
-//             end else if(o3==1'b0 && counter3 >= 400000000) begin
-//                 counter3 <= 0;
-//                 o3 <= 1'b1;
-//             end
-//             if (in3 != in3m && in3==1'b0 && o3==1'b1) begin
-//                 o3=1'b0;
-//                 counter3=0;
-//                 score <= score + 3'd1;
-//             end
+            //button 3
+            if (o3==1'b1 && counter3 >= 50000000) begin
+                counter3 <= 0;
+                o3 <= 1'b0;
+            end else if(o3==1'b0 && counter3 >= 400000000) begin
+                counter3 <= 0;
+                o3 <= 1'b1;
+            end
+            if (in3 != in3m && in3==1'b0 && o3==1'b1) begin
+                o3=1'b0;
+                counter3=0;
+                score <= score + 3'd1;
+            end
 
-//             //button 4
-//             if (o4==1'b1 && counter4 >= 50000000) begin
-//                 counter4 <= 0;
-//                 o4 <= 1'b0;
-//             end else if(o4==1'b0 && counter4 >= 350000000) begin
-//                 counter4 <= 0;
-//                 o4 <= 1'b1;
-//             end
-//             if (in4 != in4m && in4==1'b0 && o4==1'b1) begin
-//                 o4=1'b0;
-//                 counter4=0;
-//                 score <= score + 3'd1;
-//             end
+            //button 4
+            if (o4==1'b1 && counter4 >= 50000000) begin
+                counter4 <= 0;
+                o4 <= 1'b0;
+            end else if(o4==1'b0 && counter4 >= 350000000) begin
+                counter4 <= 0;
+                o4 <= 1'b1;
+            end
+            if (in4 != in4m && in4==1'b0 && o4==1'b1) begin
+                o4=1'b0;
+                counter4=0;
+                score <= score + 3'd1;
+            end
 
             //end game
             if(miss >= 10) begin
@@ -139,15 +139,15 @@
                 o5 = 1'b1;
             end
 
-//             //increment counters
-//             counter1 = counter1 + 1;
-//             in1m = in1;
-//             counter2 = counter2 + 1;
-//             in2m = in2;
-//             counter3 = counter3 + 1;
-//             in3m = in3;
-//             counter4 = counter4 + 1;
-//             in4m = in4;
+            //increment counters
+            counter1 = counter1 + 1;
+            in1m = in1;
+            counter2 = counter2 + 1;
+            in2m = in2;
+            counter3 = counter3 + 1;
+            in3m = in3;
+            counter4 = counter4 + 1;
+            in4m = in4;
 
 
             //use led to display score
